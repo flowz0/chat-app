99a4925c9c6a6eee156113c97e706eae820e2903
flowz0 <billylflowers@gmail.com>
2025-02-26 23:51:27 -0800

    Remove Warn

--- frontend/src/components/Warn.jsx
+++
@@ -1,9 +0,0 @@
-const Warn = ({ message }) => {
-  return (
-    <p className="mt-2 py-0.5 px-1.5 rounded-xl text-sm bg-red-500 text-zinc-100 w-fit">
-      {message}
-    </p>
-  )
-};
-
-export default Warn;
