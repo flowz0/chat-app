03452d41d630e9cc906e96fd6da128b20e9ea818
flowz0 <billylflowers@gmail.com>
2025-03-13 05:36:33 -0700

    Add: Sidebar, NoChat, Chat components to HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,22 @@
+import { useChatStore } from "../store/useChatStore"
+
+import ChatContainer from "../components/ChatContainer"
+import NoChatSelected from "../components/NoChatSelected"
+import Sidebar from "../components/Sidebar";
+
 const HomePage = () => {
+  const { selectedUser } = useChatStore();
   return (
-    <div>HomePage</div>
+    <div className="h-screen bg-zinc-900 text-zinc-100 flex flex-col items-center">
+      <div className="pt-28 px-6 w-full max-w-6xl h-[calc(100vh-2rem)]">
+        <div className="flex h-full rounded-lg overflow-hidden">
+          <Sidebar />
+
+          {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+
+        </div>
+      </div>
+    </div>
   )
 }
 
