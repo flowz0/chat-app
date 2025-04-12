0d1d7269bee67b2308748f407b96bc1337323ba0
flowz0 <billylflowers@gmail.com>
2025-04-12 02:13:06 -0700

    feat: subscribe/unsubscribe to messages with socket & scroll to last message

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useChatStore } from "../store/useChatStore"
 import ChatHeader from "./ChatHeader";
 import ChatInput from "./ChatInput";
@@ -7,12 +7,30 @@ import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-  const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    subscribeToMessages,
+    unsubscribeFromMessages
+  } = useChatStore();
   const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
 
   useEffect(() => {
     getMessages(selectedUser._id)
-  }, [selectedUser._id, getMessages])
+
+    subscribeToMessages();
+
+    return () => unsubscribeFromMessages();
+  }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
+
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages])
 
   if (isMessagesLoading) {
     return (
@@ -43,6 +61,7 @@ const ChatContainer = () => {
                       : selectedUser.profilePic || "/profilePicture.png"
                   }
                   alt="Profile picture"
+                  ref={messageEndRef}
                 />
               </div>
             </div>
